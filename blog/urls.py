91420c1d9f66cb1f1d37fb11977e59e47cd8fd15
eunyoung <kwkdmsdud@naver.com>
2022-12-15 10:47:23 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # 이 부분을 채울 겁니다!
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),
 ]
